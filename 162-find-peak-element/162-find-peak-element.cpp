6bc412b70bfeb72e17e3208787a66f33f82cf2a0
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-01-22 16:51:57 -0500

    Time: 10 ms (12.98%), Space: 8.8 MB (49.40%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        auto it= max_element(nums.begin(),nums.end());
-        return (it-nums.begin());
+       return binarySearch(nums,0,nums.size()-1);
+    }
+    
+    int binarySearch(vector<int>& nums, int low, int high){
+      if (low==high){
+          return low;
+      }
+            int mid = (low+high)/2;
+        
+            if(nums[mid]>nums[mid+1]){
+               return binarySearch(nums, low,mid);
+            }
+            return binarySearch(nums,mid+1,high);
+           
+        
+       
     }
 };
