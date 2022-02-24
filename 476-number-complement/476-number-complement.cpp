e36ab4781b5e83b0f814886d086bae15c62f1d2c
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-02-24 11:34:17 -0500

    Time: 3 ms (7.32%), Space: 5.9 MB (43.08%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -1,55 +1,13 @@
 #include <math.h>
 class Solution {
 public:
+int findComplement(int num) {
+   
     
-    string convertDecToBin(int n){
-       string s;
-        string r;
-        while(n){
-            if(n%2){
-                s.push_back('1');
-            }else{
-                s.push_back('0');
-            }
-            n/=2;
-            
-        }
-        while(s!=""){
-            r.push_back(s.back());
-            s.pop_back();
-        }
-        return r;
-           
-    }
-    
-    int convertBinToDec(string r){
-        int dec = 0,i=0;
-        while(r!=""){
-            char rem = r.back();
-            r.pop_back();
-         
-            dec+=((pow(2,i))*(rem-'0'));
-            cout<<rem<<" "<<dec<<endl;
-            i++;
-            
-        }
-        return dec;
-    }
-    int findComplement(int num) {
-        
-        string r = convertDecToBin(num);
-       
-      
-        for(int i =0;i<r.length();i++){
-            if(r[i]=='1'){
-                r[i] = '0';
-            }else{
-                r[i] = '1';
-            }
-        }
-        cout <<r<<endl;
-        return convertBinToDec(r);
-      
+    int mask=1;
+    while((num & mask)!=num)
+        {mask = mask<<1 | 1;}
+    return num^mask;
         
     }
 };
