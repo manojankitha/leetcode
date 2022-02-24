2b69d0a1bc9c93b92f4f19679bbf141ec66090fb
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-02-24 09:00:00 -0500

    Time: 23 ms (65.87%), Space: 16.9 MB (49.55%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,11 +3,9 @@ public:
     int singleNumber(vector<int>& nums) {
       
         
-        int xord = nums[0] ;
-            if (nums.size() == 1){
-                return xord;
-            }
-        for(int i=1;i<nums.size();i++){
+        int xord = 0 ;
+           
+        for(int i=0;i<nums.size();i++){
             xord^=nums[i];
         }
         return xord;
