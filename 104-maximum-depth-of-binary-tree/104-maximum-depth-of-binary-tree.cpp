a49798bc423e485df8ce3d224cd8fdc16014fecd
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-07 09:30:19 -0500

    Time: 11 ms (64.11%), Space: 18.8 MB (56.22%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -9,32 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
-public:
-    int calcDepth(TreeNode* root,int depth){
-        
-        int left=0,right=0;
-        if(root->left==NULL && root->right==NULL){
-            return depth;
+    public:
+        int calcDepthRecurse(TreeNode* root, int depth){
+           
+            int left = 0,right=0;
+            while(1){
+                if(root->left==NULL && root->right==NULL){
+                    return depth;
+                }
+                if(root->left){
+                    left=calcDepthRecurse(root->left,depth+1);
+                }
+                if(root->right){
+                    right=calcDepthRecurse(root->right,depth+1);
+                }
+                return max(left,right);
+            }
+            
+            
         }
-        if(root->left){
-            left = calcDepth(root->left,depth+1);
-        }
-        if(root->right){
-            right = calcDepth(root->right,depth+1);
-        }
-        return max(left,right);
-    }
     
-    int maxDepth(TreeNode* root) {
+    int calcDepthIterative(TreeNode* root){
+        stack<pair<int,TreeNode*>> st;
+        int res=0;
         if(root==NULL){
-            return 0;
+            return res;
         }
-        return calcDepth(root,1);
+        st.push(make_pair(1,root));
+        while(!st.empty()){
+            int depth = st.top().first;
+            res = max(depth,res);
+            TreeNode* node = st.top().second;
+            st.pop();
+            if(node->left){
+                st.push(make_pair(depth+1,node->left));
+            }
+            if(node->right){
+                st.push(make_pair(depth+1,node->right));
+            }
+            
+        }
+        return res;
         
         
-        
-        
-        
+    }
+    int maxDepth(TreeNode* root){
+     
+        //return calcDepthRecurse(root,1);
+        return calcDepthIterative(root);
     }
 };
+
+
+
+
+
+
