d3ae9bf61d9a7dd55557ceaca056ebcd5cd11fd7
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-09 17:06:31 -0500

    Time: 13 ms (51.52%), Space: 7.3 MB (47.01%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        unordered_map<char,int> mpS;
-        unordered_map<char,int> mpT;
+    
+    // TC:O(N)
+    // SC:O(N) - worst case
+//     bool isAnagram(string s, string t) {
+//         unordered_map<char,int> mpS;
+//         unordered_map<char,int> mpT;
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             mpS[s[i]]++;
+//             mpT[t[i]]++;        
+//         }
+
+//         if(mpS!=mpT){
+//             return false;
+//         }
+//         return true;
+        
+//     }
+    
+    
+      bool isAnagram(string s, string t) {
+        // since it is mentioned that it is just alphabets - lower case
+         int arr[26] = { };
+       
         if(s.length()!=t.length()){
             return false;
         }
         for(int i=0;i<s.length();i++){
-            mpS[s[i]]++;
-            mpT[t[i]]++;
-                
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+                 
         }
 
-        if(mpS!=mpT){
-            return false;
+        for(int count:arr){
+            if(count!=0){
+                return false;
+            }
         }
+        
         return true;
         
     }
+    
+    
+    
 };
