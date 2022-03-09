82ed512c545144bb8bc451efdda31fa7431a05cc
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-09 18:58:38 -0500

    Time: 48 ms (62.98%), Space: 24.4 MB (14.52%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
     
-    
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> out;
-        unordered_map<string,vector<string>> mp;
-        for (string str:strs){
-            string temp = str;
-            sort(str.begin(),str.end());
-            mp[str].push_back(temp);
-        }
+    // TC: O(NKlogK) where N is strs.size(), K is length of longest string
+    // SC: O(NK)
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<vector<string>> out;
+//         unordered_map<string,vector<string>> mp;
+//         for (string str:strs){
+//             string temp = str;
+//             sort(str.begin(),str.end());
+//             mp[str].push_back(temp);
+//         }
         
-        for(auto it=mp.begin();it!=mp.end();it++){
+//         for(auto it=mp.begin();it!=mp.end();it++){
+//             out.push_back(it->second);
+//         }
+//        return out;
+//     }
+  
+     // TC: O(NK) where N is strs.size(), K is length of longest string
+     // SC: O(NK)
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            vector<vector<string>> out;
+            unordered_map<string,vector<string>> mp;
+            for (string str:strs){
+                string s="";
+                string temp = str;
+                int arr[26]={0};
+                for(int i=0;i<str.length();i++){
+                    arr[str[i]-'a']++;
+                }
+                for(int j=0;j<26;j++){
+                    s+='0'+arr[j];
+                    s+='#';
+                }
+                cout<<s<<endl;
+                mp[s].push_back(temp);
+              
+        }
+               for(auto it=mp.begin();it!=mp.end();it++){
             out.push_back(it->second);
         }
        return out;
+            
     }
-  
 };
