b335f331dd51ee2d96d199fb6824da981c0341ce
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-09 20:36:18 -0500

    Time: 3 ms (35.79%), Space: 6.5 MB (10.39%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    unordered_map<char,int> symbols={{'[',-1},{'(',-2},{'{',-3},{']',1},{')',2},{'}',3}};
+    bool isValid(string s) {
+
+        stack<char> st;
+        for(char bracket:s){
+            if(symbols[bracket]<0){
+                st.push(bracket); 
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                char top=st.top();
+                st.pop();
+                if(symbols[top]+symbols[bracket]!=0){
+                    return false;
+                }
+            }
+        }
+    
+        if(st.empty()){
+            return true;
+        }else{
+            return false;     
+        }
+    }
+};
+
+
+
+//     unordered_map<char,int>symbols={{'[',-1},{'(',-2},{'{',-3},{']',1},{')',2},{'}',3}};
+//     bool isValid(string s) {
+//       stack<char>st;
+//         for(char brackets:s){
+//             if(symbols[brackets]<0){
+//                 st.push(brackets);
+//             }else{
+//                 if(st.empty())return false;
+//                 char top=st.top();
+//                 st.pop();
+//                 if(symbols[top]+symbols[brackets]!=0)
+//                 {
+//                     return false;
+//                 }
+//             }
+//         }
+//         if(st.empty())return true;
+//         return false;
+//     }
