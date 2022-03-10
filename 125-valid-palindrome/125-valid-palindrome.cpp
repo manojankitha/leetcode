9801abd06a4d7f25a9aa6339be142a174b3e7b8d
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-10 10:05:10 -0500

    Time: 6 ms (71.07%), Space: 7.4 MB (52.94%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,33 +1,30 @@
 #include<regex>
 class Solution {
 public:
-    bool isPalindrome(string s) {
-     
-        regex reg("[^A-Za-z0-9]+");
-        string S= regex_replace(s,reg,"");
-           int n = S.length();
-        cout<<S<<n;
-        if(n==1 || n==0){
-                return true;
-            }
-            
-        int front,rear;
-        for(front=0,rear=n-1;front<=rear;front++,rear--){
-            
-      
-            cout<<front<<rear;
-            if(front<=rear){
-                
-                if(tolower(S[front])!=tolower(S[rear])){
+        bool isPalindrome(string s) {
+         int start=0,end=s.length()-1;
+            while(start<end){
+                while(start<end && isalnum(s[start])==false){
+                    start++;
+                }
+                while(start<end && isalnum(s[end])==false){
+                    end--;
+                }
+                if(start>end){
                     return false;
                 }
-            } else{
+                if(tolower(s[start])!=tolower(s[end])){
+                    return false;
+                }
+                start++;
+                end--;
                 
             }
+          
+             return true;
+          
         }
-        
-       return true;
-   
-        
-    }
+    
+  
+
 };
