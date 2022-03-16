5563570a8a1ae17fdb4845173115c217819cfe98
manojankitha <58524438+manojankitha@users.noreply.github.com>
2022-03-16 18:22:01 -0400

    Time: 61 ms (75.57%), Space: 24.1 MB (30.75%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+      bool checkPalindrome(string s,int i,int j){
+         while(i<j){
+             if(s[i]!=s[j]){
+                 return false;
+             }
+             i++;
+             j--;
+         }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        if (s.length()<=1){
+            return true;
+        }
+       
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return (checkPalindrome(s,i+1,j) || checkPalindrome(s,i,j-1));
+            }
+            i++;
+            j--;
+        }
+      
+        
+    return true;
+        
+    }
+};
+
+  
