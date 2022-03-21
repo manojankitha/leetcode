bf494527629ac9b1ffd6910d94d18827dd6220fd
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-03-21 12:12:05 -0400

    Time: 124 ms (97.17%), Space: 68.8 MB (70.54%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    vector<int> findBuildings(vector<int>& heights) {
+//     vector<int> findBuildings(vector<int>& heights) {
+//         vector<int> out;
+//         stack<int> build;
+//        for(int j=heights.size()-1;j>=0;j--){
+//           while(!build.empty() && build.top()<heights[j]){
+//              build.pop();
+//           }
+//            if(build.empty()){
+//                out.push_back(j);
+//            } 
+//            build.push(heights[j]);   
+//        }
+        
+//        reverse(out.begin(),out.end());
+//         return out;
+//     }
+       vector<int> findBuildings(vector<int>& heights) {
         vector<int> out;
-        stack<int> build;
+       int max_height =-1;
        for(int j=heights.size()-1;j>=0;j--){
-          while(!build.empty() && build.top()<heights[j]){
-             build.pop();
-          }
-           if(build.empty()){
+         if(max_height<heights[j]){
+             max_height = heights[j];
                out.push_back(j);
-           } 
-           build.push(heights[j]);   
+         }
+           
        }
         
        reverse(out.begin(),out.end());
