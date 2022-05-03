7e71706720071b9f39c3a1c421c565c72631a62b
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-05-03 06:19:18 -0400

    Time: 27 ms (20.16%), Space: 8.2 MB (19.20%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-         // we know that our uniqueSet has to be of max length 2
-        int start,end;
+        int start=0,end=0;
+        int lenLongestSub = 0;
         int stringLen = s.length();
-        unordered_set<char> uniqueSet;
-        int lenLongestSubString=0;
-        unordered_map<char,int> freq;
+        if(stringLen<k){
+            return stringLen;
+        }
         if(k==0){
             return 0;
         }
-       if(stringLen<=k){
-           return stringLen;
-       }
-      
+        
+        unordered_set<char> uniqueSet;
+        unordered_map<char,int> freq;
         
         for(start=0,end=0;start<=end && end<stringLen;end++){
-            
-          if (uniqueSet.find(s[end])==uniqueSet.end()){
-                while(uniqueSet.size()==k){
-                    // I need to increment start
-                    if(freq[s[start]]==1){
-                        // if freq is currently one, if we remove this element from sliding window we                           need to remove from freq map and uniqueSet
-                        freq.erase(s[start]);
-                        uniqueSet.erase(s[start]);
-                    }else{
-                        // just decrement freq in this case
-                        freq[s[start]]--;
-                    }
-                    start++;
-                }      
-                uniqueSet.insert(s[end]);
-                freq[s[end]]=1;
-            } else{
-                
-                freq[s[end]]++;
-            }
-            
-                       
-            if(end-start+1 >=lenLongestSubString){
-                lenLongestSubString = end-start+1;
-            }
-                
-        }     
-        return lenLongestSubString;  
-        
-    }
+             if(uniqueSet.find(s[end])==uniqueSet.end()){
+                 // new unique character
+                 while(uniqueSet.size()==k){
+                     if(freq[s[start]]==1){
+                         freq[s[start]]=0;
+                         uniqueSet.erase(s[start]);
+                     } else {
+                         freq[s[start]]--;
+                     }
+                     start++;
+                 }
+                 uniqueSet.insert(s[end]);
+                 freq[s[end]]=1;
+             } else {
+                 freq[s[end]]++;
+             }
+
+             if(end-start+1 >= lenLongestSub){
+                 lenLongestSub = end-start+1;
+             }    
+         }
+            return lenLongestSub;
+        }  
 };
