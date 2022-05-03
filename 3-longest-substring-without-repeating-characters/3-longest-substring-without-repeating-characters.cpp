e8ba70003679c127cbc97727b926e6399ee36f92
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-05-03 07:34:40 -0400

    Time: 52 ms (21.86%), Space: 12.3 MB (19.28%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     // TC:O(N) 
     int lengthOfLongestSubstring(string s) {
-        int start,end;
-        int stringLen = s.length();
+        int start=0,end=0,stringLen = s.length();
+        int lenLongestSub = 0;
         unordered_set<char> uniqueSet;
-        int lenLongestSubString=0;
-        if(stringLen<=1){
-            return stringLen;
-        }
-        for (start=0,end=0;(start<=end && end<s.length());end++){
-            while(uniqueSet.find(s[end])!=uniqueSet.end()){
-              
-                uniqueSet.erase(s[start]);
-                start++;
-            }
-            uniqueSet.insert(s[end]);
-            if(end-start+1 >=lenLongestSubString){
-                lenLongestSubString = end-start+1;
-            }
-            
-        }
-        return lenLongestSubString; 
+        unordered_map<char,int> freq;
         
+        for(start=0,end=0;start<=end && end<stringLen;end++){
+            while (uniqueSet.find(s[end])!=uniqueSet.end()){
+              // repeat char
+                if(freq[s[start]]==1){
+                    freq[s[start]]=0;
+                    uniqueSet.erase(s[start]);
+                } else {
+                    freq[s[start]]--;
+                }
+                start++;
+                
+            } 
+                 // new unique char
+                uniqueSet.insert(s[end]);
+                freq[s[end]]++;
+            
+            
+            
+            if(end-start+1 > lenLongestSub){
+                lenLongestSub = end-start+1;
+            }
+        }
+        return lenLongestSub;
     }
 };
