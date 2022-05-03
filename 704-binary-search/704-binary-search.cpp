afccbfeecd8a299f796ca0e5a2db708bebe72752
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-05-03 08:20:13 -0400

    Time: 58 ms (30.72%), Space: 27.5 MB (63.92%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -8,18 +8,35 @@ public:
     }
     
     // recursive search
+    // int binarySearch(vector<int>& nums, int l, int r, int target){
+    //     if(r>=l){
+    //         int mid = l+(r-l)/2;
+    //         if(nums[mid]==target){
+    //             return mid;
+    //         }
+    //         if(nums[mid]<target){
+    //             return binarySearch(nums,mid+1,r,target);
+    //         } else {
+    //             return binarySearch(nums,0,mid-1,target); 
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    // iterative search
     int binarySearch(vector<int>& nums, int l, int r, int target){
-        if(r>=l){
-            int mid = l+(r-l)/2;
+        while(l<=r){
+            int mid = l+ (r-l)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[mid]<nums[r]){
-                return binarySearch(nums,mid+1,r,target);
+            if(nums[mid]<target){
+                l = mid+1;
             } else {
-                return binarySearch(nums,0,mid-1,target); 
+                r = mid-1;
             }
         }
         return -1;
     }
+    
 };
