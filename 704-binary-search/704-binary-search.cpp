c554bfc185653418916daad2f17e6e8dae5d8b03
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-05-03 08:20:28 -0400

    Time: 91 ms (5.41%), Space: 27.5 MB (63.92%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -8,35 +8,35 @@ public:
     }
     
     // recursive search
-    // int binarySearch(vector<int>& nums, int l, int r, int target){
-    //     if(r>=l){
-    //         int mid = l+(r-l)/2;
-    //         if(nums[mid]==target){
-    //             return mid;
-    //         }
-    //         if(nums[mid]<target){
-    //             return binarySearch(nums,mid+1,r,target);
-    //         } else {
-    //             return binarySearch(nums,0,mid-1,target); 
-    //         }
-    //     }
-    //     return -1;
-    // }
-    
-    // iterative search
     int binarySearch(vector<int>& nums, int l, int r, int target){
-        while(l<=r){
-            int mid = l+ (r-l)/2;
+        if(r>=l){
+            int mid = l+(r-l)/2;
             if(nums[mid]==target){
                 return mid;
             }
             if(nums[mid]<target){
-                l = mid+1;
+                return binarySearch(nums,mid+1,r,target);
             } else {
-                r = mid-1;
+                return binarySearch(nums,0,mid-1,target); 
             }
         }
         return -1;
     }
+    
+    // iterative search
+    // int binarySearch(vector<int>& nums, int l, int r, int target){
+    //     while(l<=r){
+    //         int mid = l+ (r-l)/2;
+    //         if(nums[mid]==target){
+    //             return mid;
+    //         }
+    //         if(nums[mid]<target){
+    //             l = mid+1;
+    //         } else {
+    //             r = mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
     
 };
