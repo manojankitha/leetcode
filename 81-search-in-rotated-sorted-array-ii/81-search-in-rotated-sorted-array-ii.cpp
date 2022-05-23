69c46db70e82a1f992bde3a688005c6f8b3514d9
Ankitha Manoj <58524438+manojankitha@users.noreply.github.com>
2022-05-23 16:56:55 -0400

    Time: 9 ms (47.78%), Space: 14.1 MB (31.07%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -4,30 +4,56 @@ public:
          
         int start=0;
         int end = nums.size()-1;
-        while(start<=end){
-          
-            while(start< end && nums[start]==nums[start+1]) {start++;}
-            while( start < end && nums[end]==nums[end-1]) {end--;}
-          
-            int mid = start+(end-start)/2;
-            if(nums[mid]==target){
+         while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){// if we find target
                 return true;
             }
-            else if (nums[mid]>= nums[start]){
-                // first section
-                if(target > nums[mid] || target<nums[start]){
-                    start=mid+1;
-                } else {
-                    end=mid-1;
-                }    
-            } else {
-               if(target < nums[mid] || target> nums[end]){
-                    end=mid-1;
-                } else {
-                    start=mid+1;
-                }  
+            else if(nums[mid]==nums[end]&&nums[mid]==nums[start]){// to avoid conflict 
+                end--;
+                start++;
             }
-        }   
+            else if(nums[mid]>=nums[start]){// if left array is sorted
+                if(target>=nums[start]&&target<=nums[mid]){// if target is in the range of start--to--mid
+                    end=mid-1;
+                }
+                else{// else we find in the range of mid---to---end
+                    start=mid+1;
+                }
+            }
+            else{// if right array is sorted 
+               if(target>=nums[mid]&&target<=nums[end]){// if target is in the of mid---to---right
+                   start=mid+1;
+                }
+                else{// then we search in left range
+                    end=mid-1;
+                }
+            }
+        }
+//         while(start<=end){
+          
+//             while(start< end && nums[start]==nums[start+1]) {start++;}
+//             while( start < end && nums[end]==nums[end-1]) {end--;}
+          
+//             int mid = start+(end-start)/2;
+//             if(nums[mid]==target){
+//                 return true;
+//             }
+//             else if (nums[mid]>= nums[start]){
+//                 // first section
+//                 if(target > nums[mid] || target<nums[start]){
+//                     start=mid+1;
+//                 } else {
+//                     end=mid-1;
+//                 }    
+//             } else {
+//                if(target < nums[mid] || target> nums[end]){
+//                     end=mid-1;
+//                 } else {
+//                     start=mid+1;
+//                 }  
+//             }
+//         }   
         return false;
         
     }
